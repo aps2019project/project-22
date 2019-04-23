804f8e681c3c7cc63b9f6a6ef57e22f41d80df7c
atoosachegini1 <45598899+atoosachegini1@users.noreply.github.com>
2019-04-23 15:58:34 +0430

    Add files via upload
    classes Hand and Deck and Player and Account

---
+++ src/Model/Hand.java
@@ -0,0 +1,45 @@
+package Model;
+import java.util.ArrayList;
+
+public class Hand extends Card{
+    private Arraylist<Card> cards;
+    private int turn;
+
+    public Arraylist<Card> getCards () {
+        return cards;
+    }
+
+    public void chooseCards() {
+
+    }
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn (int turn){
+
+    }
+
+    public void chooseCards(Arraylist<Card> Deck.getCards){
+
+    }
+
+    public boolean checkIfTheCardsAreInDeck(Arraylist<Card> Deck.getCards){
+
+    }
+    public void deleteFromDeck(Deck deck){
+
+    }
+
+    public boolean checkIfHandHasFreeSpace(cards){
+
+    }
+    public void addToHand(Deck.getCards){
+
+    }
+
+    public void deleteFromHand(Card card){
+
+    }
+
+}
