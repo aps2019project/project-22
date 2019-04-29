92de6209553050ee36b341824608686eda8d16e7
matinamehdizadeh <45507606+matinamehdizadeh@users.noreply.github.com>
2019-04-29 12:40:25 +0430

    match history

---
+++ MatchHistory.java
@@ -0,0 +1,26 @@
+package Model;
+
+public class MatchHistory {
+    private int time;
+    private int numberOfWins;
+    private String name;
+    public void setTime(int number){
+        this.time = number;
+    }
+    public void setNumberOfWins(int number){
+        this.numberOfWins = number;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getTime(){
+        return  time;
+    }
+    public int getNumberOfWins(){
+        return numberOfWins;
+    }
+    public String getName(){
+        return name;
+    }
+
+}
