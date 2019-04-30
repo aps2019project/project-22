058b666fef409db83fba8182c375f2f2979d28a2
WorkshopAPGH <yasamanshaykhan@gmail.com>
2019-05-01 02:37:40 +0430

    menu

--- src/Presenter/Main.java
+++ src/Presenter/Main.java
@@ -1,7 +1,14 @@
 package Presenter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        menu(scanner);
+    }
+
+    public static void menu(Scanner scanner) {
 
     }
 }
