4fdcf0d419fa46af97ffc0f3eb7ebf0616b21e00
WorkshopAPGH <yasamanshaykhan@gmail.com>
2019-05-03 02:19:30 +0430

    add price in show cards and items

--- src/View/Show.java
+++ src/View/Show.java
@@ -13,14 +13,15 @@ public class Show {
             System.out.println("\t" + number + " : Name : " + collection.getHeroes().get(i).getName() + " - AP : " +
                     collection.getHeroes().get(i).getAttackPower() + " - HP : " + collection.getHeroes().get(i).
                     getHealthPoint() + " - Class : " + collection.getHeroes().get(i).getTypeOfAttack() +
-                    " - Special power: " + "attack");                                   //attack nmidunm jash chie!
+                    " - Special power: " + "attack" + " - price: "+collection.getHeroes().get(i).getPrice());
+            //attack nmidunm jash chie!
             number++;
         }
         number = 1;
         System.out.println("Items :");
         for (int i = 0; i < collection.getItems().size(); i++) {
             System.out.println("\t" + number + " : Name : " + collection.getItems().get(i).getName() + " Desc: " +
-                    collection.getItems().get(i).getDescription());
+                    collection.getItems().get(i).getDescription()+" - price: "+collection.getItems().get(i).getPrice());
             number++;
         }
         number = 1;
@@ -28,7 +29,7 @@ public class Show {
         for (int i = 0; i < collection.getSpells().size(); i++) {
             System.out.println("\t" + number + " : Type : Spell - Name : " + collection.getSpells().get(i).getName() +
                     " - MP : " + collection.getSpells().get(i).getMana() + " - " +
-                    collection.getSpells().get(i).getDescription());
+                    collection.getSpells().get(i).getDescription()+" - price: "+collection.getSpells().get(i).getPrice());
             number++;
         }
         for (int i = 0; i < collection.getMinions().size(); i++) {
@@ -36,7 +37,7 @@ public class Show {
                     + " - Class: " + collection.getMinions().get(i).getTypeOfAttack() + " - AP : " + collection.
                     getMinions().get(i).getAttackPower() + " - HP : " + collection.getMinions().get(i).getHealthPoint()
                     + " - MP : " + collection.getMinions().get(i).getManaPoint() + " - Special power : " +
-                    collection.getMinions().get(i).getSpecialPower());
+                    collection.getMinions().get(i).getSpecialPower()+" - price: "+collection.getMinions().get(i).getPrice());
             number++;
         }
     }
