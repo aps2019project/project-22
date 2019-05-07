337d847a473593d62cac3cb32a82f9c3804e48ce
matinamehdizadeh <45507606+matinamehdizadeh@users.noreply.github.com>
2019-05-08 02:39:22 +0430

    add custom game

--- src/Model/CustomGame.java
+++ src/Model/CustomGame.java
@@ -4,6 +4,17 @@ public class CustomGame extends Battle {
     public void setHero(Hero hero, Player player){
         player.setHero(hero);
     }
+    public void setMode(int number){
+        mode = number;
+    }
+    public void setCoustomGame(String string){
+        String split[] = string.split(" ");
+        player1.setMainDeck(Deck.searchDeckByName(split[2]));
+        mode = Integer.parseInt(split[3]);
+        if (mode == 3){
+            howManyFlags = Integer.parseInt(split[4]);
+        }
+    }
     public void checkForWinner(){
         if (mode == 1){
             if (player1.getHero().getHealthPoint() <= 0){
