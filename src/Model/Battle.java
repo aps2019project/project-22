ea110776b75d5b428bcca21c76ae7ea26ef05e6f
WorkshopAPGH <yasamanshaykhan@gmail.com>
2019-06-29 16:27:44 +0430

    bug fixed

--- src/Model/Battle.java
+++ src/Model/Battle.java
@@ -1296,7 +1296,7 @@ public class Battle {
                             player.setMana(9);
                         }
                     }
-//                    Platform.requestNextPulse();
+                    Platform.requestNextPulse();
 
                     singlePlayerTurn();
 
