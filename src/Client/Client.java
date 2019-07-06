270a1a0f80199b682fa1ea03e0201d4272e1b971
WorkshopAPGH <yasamanshaykhan@gmail.com>
2019-07-06 19:32:39 +0430

    update client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -2,6 +2,8 @@ package Client;
 
 import Model.*;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -9,17 +11,23 @@ import java.util.Scanner;
 import java.util.Formatter;
 
 public class Client {
+
     private static ArrayList<Client> clients = new ArrayList<>();
+    private static DataInputStream input = null;
+    private static DataOutputStream out = null;
+    private static Socket socket;
     private Account account;
     private Thread clientThread;
-    private Socket socket;
     private Formatter formatter;
     private Scanner scanner;
 
     public Client(Socket socket) {
         this.setSocket(socket);
+        System.out.println("Connected");
         clients.add(this);
         try {
+            input = new DataInputStream(System.in);
+            out = new DataOutputStream(socket.getOutputStream());
             formatter = new Formatter(socket.getOutputStream());
             scanner = new Scanner(socket.getInputStream());
         } catch (IOException e) {
@@ -54,5 +62,26 @@ public class Client {
 
     public void setClientThread(Thread clientThread) {
         this.clientThread = clientThread;
+    }
+
+    public static void main(String[] args) {
+        String line = "";
+
+        while (!line.equals("End")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
     }
 }
